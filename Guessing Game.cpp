3188d79e8ebe61ecfdddcc1b29be9c7c13580fe9
Abhijeet Mourya <67868256+abhijeetmourya@users.noreply.github.com>
2020-09-04 10:44:18 +0530

    Update Guessing Game.cpp

--- Guessing Game.cpp
+++ Guessing Game.cpp
@@ -8,7 +8,7 @@ int main()
     int guess;
     int guesscount;
 
-    while(guess != secret && guesscount != 3){               //Prompts the user for a number until it's correct
+    while(guess != secret && guesscount != 3){               //Prompts the user for a number until it's correct or they are out of guess
         cout<<"Guess a number(1 to 9): ";
         cin>>guess;
         guesscount++;
@@ -16,7 +16,7 @@ int main()
     if(guesscount < 3){
         cout<<"You Win!";
     }else
-    cout<<"You Lose:(";      //Informs the user if the guess is correct or wrong
+    cout<<"You Lose:(";                                    //Informs the user if the guess is correct or wrong
 
     return 0;
 }
